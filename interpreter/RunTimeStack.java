6fc5dbb08e6ed489bafc194d8cee26145e3f07b9
Mesoma Esonwune <mesonwune@gmail.com>
2019-03-06 22:37:33 -0800

    Started RunTimeStack

--- interpreter/RunTimeStack.java
+++ interpreter/RunTimeStack.java
@@ -7,6 +7,7 @@ public class RunTimeStack {
 
     private ArrayList<Integer> runTimeStack;
     private Stack<Integer> framePointer;
+    private int size;
 
     public RunTimeStack() {
         runTimeStack = new ArrayList<>();
@@ -14,6 +15,49 @@ public class RunTimeStack {
         // Add initial Frame Pointer, main is the entry
         // point of our language, so its frame pointer is 0.
         framePointer.add(0);
+        size =  runTimeStack.size();
     }
-    
+
+    public void dump()
+    {
+
+    }
+
+    public int peek()
+    {
+        return runTimeStack.get(size - 1);
+    }
+
+    public int pop()
+    {
+        int top = peek();
+        runTimeStack.remove(size - 1);
+        return top;
+    }
+
+    //i have no clue what this means
+    public void newFrameAt(int offset)
+    {
+
+    }
+
+    public void popFrame()
+    {
+
+    }
+
+    public int store(int offset)
+    {
+        return 0;
+    }
+
+    public int load(int offset)
+    {
+        return 0;
+    }
+
+    public Integer push(Integer val)
+    {
+
+    }
 }
